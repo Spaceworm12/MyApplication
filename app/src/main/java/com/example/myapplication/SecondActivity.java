f539e9614e4c363541ff3799b4c027e342a716f5
Dmitry Bekasov <dmitry.bekasov@x5.ru>
2023-03-09 17:00:48 +0300

    Ревью

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -5,20 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        //удалить
         LinearLayout linearLayout = new LinearLayout(this);
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.drawable.ic_launcher_foreground);
+
+        Button backToTheFirstScreen = findViewById(R.id.backToTheFirstScreen);
+
+        backToTheFirstScreen.setOnClickListener(this);
+
     }
+
     public void onClick(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+        int currentView = view.getId();
+        //пример
+        if (currentView == R.id.backToTheFirstScreen) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        } else if (currentView == R.id.thisIsImage) {
+            //что-нибудь
+        }
     }
 }
+
