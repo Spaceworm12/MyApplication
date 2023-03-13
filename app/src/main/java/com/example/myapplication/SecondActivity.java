1eea73b21aba152534ca80bf92d860671686999e
Spaceworm12 <127404549+Spaceworm12@users.noreply.github.com>
2023-03-13 21:01:54 +0300

    few little work

--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -1,22 +1,23 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.Button;
 
-public class SecondActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button b = (Button) findViewById(R.id.backToTheFirstScreen);
+        b.setOnClickListener(this);
     }
+
     public void onClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
-        finish();
+        this.finish();
     }
 }
