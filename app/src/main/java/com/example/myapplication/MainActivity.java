f021bb9798c0fff30a90610c9af59671ee3c1cb7
Viktor Radchenko <spaceworm1@yandex.ru>
2023-03-20 17:56:20 +0300

    Dima`s work

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.myapplication;
 
+import static java.util.jar.Pack200.Packer.PASS;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final int REQUEST_CODE_SETTING_ACTIVITY = 99;
+    String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +21,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button b = (Button) findViewById(R.id.goToTheNextScreenButton);
         b.setOnClickListener(this);
+
+//        if(getIntent() != null) {
+//            String text = getIntent().getExtras().getString(PASS);
+//            if (text == null) {
+//                setData("");
+//            } else {
+//                setData(text);
+//            }
+//        }
     }
 
     public void onClick(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_SETTING_ACTIVITY);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode != REQUEST_CODE_SETTING_ACTIVITY) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+        if (resultCode == RESULT_OK) {
+            assert data != null;
+            s = data.getExtras().getString(PASS);
+            if (s == null) {
+                setData("");
+            } else {
+                setData(s);
+            }
+        }
+    }
+
+    private void setData(String s) {
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(s);
     }
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -2,22 +2,55 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String PASS = "PASS";
+    private String z = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button b = (Button) findViewById(R.id.backToTheFirstScreen);
         b.setOnClickListener(this);
+        EditText editText = findViewById(R.id.editText);
+        TextView textView = findViewById(R.id.textView);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                TextView textView = findViewById(R.id.textView);
+                textView.setText(s);
+                z=s.toString();
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 
     public void onClick(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent();
+        intent.putExtra(PASS,z);
+        setResult(RESULT_OK, intent);
+
         this.finish();
     }
+
 }
