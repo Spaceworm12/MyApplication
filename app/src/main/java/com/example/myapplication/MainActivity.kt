ea567fd4a572f58449cf45e2e1399ba3e8c4bee6
Viktor Radchenko <spaceworm1@yandex.ru>
2023-04-11 09:28:37 +0300

    after review fix!

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import First
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
